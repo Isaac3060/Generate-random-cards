bb52511528eea5e7cfc372c87be4513bacc872d1
Isaac3060 <isaacmanuel2299@gmail.com>
2020-05-06 19:29:46 +0000

    first commit

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ import random
 suites = ['♡', '♢', '♤', '♧']
 values = list(range(1,14))
 
-def get_random_card():
+def get_random_card(list1,list2):
     # your code here
-    return "3♤"
-    
-print(get_random_card())
+
+    suite_index = random.randint(0,len(list1)-1)
+    values_index = random.randint(0,len(list2)-1)
+
+    return list1[suite_index] + " " + str(list2[values_index])
+  
+print(get_random_card(suites,values))
+
